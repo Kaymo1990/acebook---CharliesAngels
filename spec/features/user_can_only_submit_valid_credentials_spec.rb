dba0bb92006f115aeebc6f10495fe5a888ad3bf8
Will Howes <williameldenhowes@gmail.com>
2019-08-22 15:37:26 +0100

    Sign up page styled

--- spec/features/user_can_only_submit_valid_credentials_spec.rb
+++ spec/features/user_can_only_submit_valid_credentials_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Sign up credentials", type: :feature do
     visit "/users/new"
     fill_in "Firstname", with: " "
     fill_in "Lastname", with: " "
-    fill_in "Email", with: "testexample.com"
+    fill_in "inputEmail", with: "test@example.com"
     fill_in "Password", with: "pass"
     click_button "Submit"
     expect(page).not_to have_content("Welcome to Acebook")
