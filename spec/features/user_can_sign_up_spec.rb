badfe6137037025a4a49dc9f0f85fc069b3b8d67
Will Howes <williameldenhowes@gmail.com>
2019-08-22 16:23:56 +0100

    Tests passing but two tests fail if you run again (need to look into resetting database before these tests)

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 require_relative './sign_up_helper'
 
 RSpec.feature "Sign Up", type: :feature do
-  xscenario "User can sign up" do
+  scenario "User can sign up" do
     registration
-    expect(page).to have_content("Welcome to Acebook")
+    expect(page).to have_content("Test")
   end
 end
