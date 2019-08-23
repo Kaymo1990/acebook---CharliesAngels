d230a461234df4397c6b9fd742fc3ba9c92ff461
Charles Kettell <charleskettell@hotmail.co.uk>
2019-08-23 13:06:00 +0100

    search page added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p 'DB Seeded'
+
+params = {"firstname"=>"Kay", "lastname"=>"Mo", "email"=>"k@mo.com", "password"=>"password"}
+
+100.times do
+  User.create([{
+    "firstname"=>Faker::Name.first_name,
+    "lastname"=>Faker::Name.last_name,
+    "email"=>Faker::Internet.email,
+    "password"=>"password"
+    }])
+end
